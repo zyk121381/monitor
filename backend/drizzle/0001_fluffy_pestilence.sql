49d03771199503524181e2d3d3e7d2108ff79fc5
zaunist <y.bz@foxmail.com>
2025-06-12 22:01:40 +0800

    chore: db migrate

---
+++ backend/drizzle/0001_fluffy_pestilence.sql
@@ -0,0 +1,7 @@
+ALTER TABLE `agents` DROP COLUMN `cpu_usage`;--> statement-breakpoint
+ALTER TABLE `agents` DROP COLUMN `memory_total`;--> statement-breakpoint
+ALTER TABLE `agents` DROP COLUMN `memory_used`;--> statement-breakpoint
+ALTER TABLE `agents` DROP COLUMN `disk_total`;--> statement-breakpoint
+ALTER TABLE `agents` DROP COLUMN `disk_used`;--> statement-breakpoint
+ALTER TABLE `agents` DROP COLUMN `network_rx`;--> statement-breakpoint
+ALTER TABLE `agents` DROP COLUMN `network_tx`;
